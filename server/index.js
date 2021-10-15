dfb808dbaab07bab94e4634560105a55a79ef3b5
Andrew Hang <Andrewhang94@gmail.com>
2021-10-15 11:45:52 -0700

    fix placeholder server file Co-authored-by: Spencer Lepine <spencerlepine26@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
-require('dotenv').config();
-const server = require('./server.js');
+const config = require('./config/config');
+const server = require('./server');
 
 // Vite Setup Goes here ...
 
 // server.listen(process.env.DEV_PORT, () => {
 //   console.log(`Server is currently listening on http://localhost:${process.env.DEV_PORT}`);
 // });
+
+const app = express();
+
+const PORT = config.PORT || 8000;
+
+if (config.NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}
+
+module.exports.app = app;
