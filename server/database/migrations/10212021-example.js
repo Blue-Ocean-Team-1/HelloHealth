f8c119626293a013362ed351d033b6d97f956ec9
Andrew Hang <Andrewhang94@gmail.com>
2021-10-15 15:37:59 -0700

    fix database migration name

--- server/database/migrations/10212021-example.js
+++ server/database/migrations/10212021-example.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => (
-    queryInterface.createTable('style', {
+    queryInterface.createTable('example', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
