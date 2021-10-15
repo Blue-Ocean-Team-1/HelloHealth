e32d2160b203bf4db0d0319854ba8cd547c0ffd6
Andrew Hang <Andrewhang94@gmail.com>
2021-10-15 11:18:10 -0700

    set up sequelize ORM

---
+++ server/config/config.js
@@ -0,0 +1,31 @@
+/*
+ * Use "joi" library to verify
+ * enviroment variables exists
+ * and match expected value/type
+ */
+
+const dotenv = require('dotenv');
+const path = require('path');
+const Joi = require('joi');
+
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+const envVarsSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+    PORT: Joi.number().default(3000),
+    DATABASE_URL: Joi.string().required().description('PostgreSQL connection string'),
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+module.exports = {
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+  DATABASE_URL: envVars.DATABASE_URL,
+};
