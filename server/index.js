c41f852b714d7f03144b2ca105ed434911652020
Andrew Hang <Andrewhang94@gmail.com>
2021-10-15 11:42:16 -0700

    add remaining server files Co-authored-by: name <spencerlepine26@gmail.com>

---
+++ server/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const server = require('./server.js');
+
+// Vite Setup Goes here ...
+
+// server.listen(process.env.DEV_PORT, () => {
+//   console.log(`Server is currently listening on http://localhost:${process.env.DEV_PORT}`);
+// });
