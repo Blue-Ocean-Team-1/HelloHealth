39e8060a795c3d8277cfb0c53280c3f6c510e209
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-16 15:31:33 -0700

    add logout button

--- client/src/components/Pages/UserAccountPage.jsx
+++ client/src/components/Pages/UserAccountPage.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import useAuth from '../../context/AuthContext.jsx';
 
-export default function UserAccountPage() {
+const UserAccountPage = () => {
+  const { logoutUser } = useAuth();
+
   return (
     <>
       <h1>UserAccountPage</h1>
+
+      <Button onClick={() => logoutUser()}>Log Out</Button>
     </>
   );
-}
+};
+
+export default UserAccountPage;
