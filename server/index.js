b40f1c2bcda818d1d7cc40dcff7956747c146d58
Andrew Hang <Andrewhang94@gmail.com>
2021-10-15 18:27:51 -0700

    Completed Vite Setup and Express server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
+require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
 const config = require('./config/config');
-const server = require('./server');
 
-// Vite Setup Goes here ...
-
-// server.listen(process.env.DEV_PORT, () => {
-//   console.log(`Server is currently listening on http://localhost:${process.env.DEV_PORT}`);
-// });
-
+const server = express();
 const PORT = config.PORT || 8000;
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+server.use(cors(corsOptions));
+// app.use(express.static(path.join(__dirname, 'dist', 'client')));
+
+server.get('/hello', (req, res) => {
+  res.status(200).send('hello');
+});
 
 if (config.NODE_ENV !== 'test') {
   server.listen(PORT, () => {
--- server/server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const morgan = require('morgan');
-
-const server = express();
-
-module.exports = server;
