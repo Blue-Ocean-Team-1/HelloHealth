9e0c0daba6c0cc3267ad7be38b2d3505295e96a4
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-16 12:59:35 -0700

    remove old config file

--- client/config/pageRoutes.js
+++
@@ -1,8 +0,0 @@
-// URL constants for each page
-
-export const HOME = '/';
-export const BOX = '/box';
-export const FARMS = '/farms';
-export const CART = '/cart';
-export const LOGIN = '/login';
-export const ACCOUNT = '/account';
