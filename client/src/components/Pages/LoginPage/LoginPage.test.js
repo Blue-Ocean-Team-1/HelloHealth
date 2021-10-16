ab6726d0af6ee16294b6e3600f7246c1d7b0916f
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-16 15:31:06 -0700

    style LoginPage form Co-authored-by: Cassandra Barragan <cassbarragan@gmail.com> Co-authored-by: Spencer Lepine <spencerlepine26@gmail.com>

---
+++ client/src/components/Pages/LoginPage/LoginPage.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import LoginPage from './LoginPage.jsx';
+import {
+  render,
+  screen,
+  fireEvent,
+  getByLabelText,
+} from '../../../../test-utils';
+
+describe('<Page />', () => {
+  beforeEach(() => {
+    render(<LoginPage />);
+  });
+
+  // describe("when page is initialized", () => {
+  //   it("then shows the light theme by default", () => {
+  //     // "Use Dark Theme" text is only shown when the light theme is active
+  //     expect(screen.getByText(/Use Dark Theme/i)).toBeTruthy();
+  //   });
+  // });
+
+  // describe("when the toggle theme button is clicked", () => {
+  //   beforeEach(() => {
+  //     userEvent.click(screen.getByText(/Use Dark Theme/i));
+  //   });
+
+  //   it("then uses the dark theme", () => {
+  //     // "Use Light Theme" text is only shown when the dark theme is active
+  //     expect(screen.getByText(/Use Light Theme/i)).toBeTruthy();
+  //   });
+  // });
+});
