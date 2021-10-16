90c9b7ac938102d28e057683dcfce10181aabdf9
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-16 09:09:54 -0700

    implement React Router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App.jsx';
 import { MainProvider } from './context/MainContext.jsx';
 
 ReactDOM.render(
-  <MainProvider>
-    <App />
-  </MainProvider>,
+  <Router>
+    <MainProvider>
+      <App />
+    </MainProvider>
+  </Router>,
   document.getElementById('root'),
 );
