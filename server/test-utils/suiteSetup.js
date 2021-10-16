b34345a4fb7998d353f92b81d5956e64bdd491b8
Andrew Hang <Andrewhang94@gmail.com>
2021-10-15 18:51:04 -0700

    changed path for sequelize server

--- server/test-utils/suiteSetup.js
+++ server/test-utils/suiteSetup.js
@@ -3,7 +3,7 @@
  */
 
 const { sequelize } = require('../database');
-const server = require('../server');
+const server = require('../index');
 
 // Close the entire Sequalize Connection after ALL tests
 // https://stackoverflow.com/questions/60217417/jest-tests-hang-due-to-open-sequelize-connections/60267873#60267873
