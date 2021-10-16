3910ccaffe4db1413ee3e0fec0b6e55c26bda7e3
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-16 13:00:47 -0700

    add updated vite config

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import dotenv from 'dotenv';
 import react from '@vitejs/plugin-react';
 
+import fs from 'fs';
+
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-  dotenv.config({
-    path: './.env',
-  });
+  dotenv.config({ path: './env' });
 
   return {
+    optimizeDeps: { include: ['firebase/app', 'firebase/firestore'] },
     plugins: [react()],
     root: 'client',
   };
