35b9d27effd3e06b9db6ab34fd2bf979bbef6e22
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-18 09:29:38 -0700

    create user account page

--- client/src/components/Pages/UserAccountPage.jsx
+++ client/src/components/Pages/UserAccountPage.jsx
@@ -3,11 +3,28 @@ import Button from '@mui/material/Button';
 import useAuth from '../../context/AuthContext.jsx';
 
 const UserAccountPage = () => {
-  const { logoutUser } = useAuth();
+  const { logoutUser, currentUser } = useAuth();
+
+  const name = currentUser.displayName || 'Anonymous'; // stored in Firebase
+  const joinDate = '7 days ago';
 
   return (
     <>
-      <h1>UserAccountPage</h1>
+      <h1>Account Details</h1>
+
+      <figure>
+        <img></img>
+        <button>Remove</button>
+      </figure>
+
+      <figure>
+        <h4>Referall Coupon: ${'$20'}</h4>
+        <button>Share Code</button>
+      </figure>
+
+      <h5>{name}</h5>
+
+      <p>Joined: {joinDate}</p>
 
       <Button onClick={() => logoutUser()}>Log Out</Button>
     </>
