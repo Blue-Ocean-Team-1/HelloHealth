bb4b3f8e3e4edaf01ed3ed46f0b0494ae956ae46
Andrew Hang <Andrewhang94@gmail.com>
2021-10-19 16:57:14 -0700

    Accepted incoming change

--- client/src/components/Product/NutritionModal.jsx
+++ client/src/components/Product/NutritionModal.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import * as React from 'react';
-=======
 import React, { useState } from 'react';
->>>>>>> main
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -14,11 +10,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-<<<<<<< HEAD
-  width: '70%',
-=======
   width: '40vmax',
->>>>>>> main
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -26,21 +18,13 @@ const style = {
 };
 
 export default function NutritionModal() {
-<<<<<<< HEAD
-  const [open, setOpen] = React.useState(false);
-=======
   const [open, setOpen] = useState(false);
->>>>>>> main
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   return (
     <div>
-<<<<<<< HEAD
-      <Button onClick={handleOpen} jus='center'>Nutrition</Button>
-=======
       <Button onClick={handleOpen}>Nutrition</Button>
->>>>>>> main
       <Modal
         open={open}
         onClose={handleClose}
