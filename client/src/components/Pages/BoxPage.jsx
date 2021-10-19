f4ba543efc2a575cd3bba927cbf5b2ea5a2bb560
Lawrence Sun <alawrencesun@gmail.com>
2021-10-19 14:22:54 -0700

    Put box title and list in the same "box" and changed NutritionModal to fit in mobile and Desktop pages
    Co-Author: Bradley Caliva bcaliva21@gmail.com

--- client/src/components/Pages/BoxPage.jsx
+++ client/src/components/Pages/BoxPage.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { FixedSizeList } from 'react-window';
-import AutoSizer from 'react-virtualized-auto-sizer';
 import MealList from '../Product/MealList.jsx';
 import Nutrition from '../Product/Nutrition.jsx';
 import useStyles from '../Product/nutritionStyles';
@@ -24,8 +23,8 @@ export default function BoxPage() {
       <>
         <ListItem style={style} key={index} component="div" disablePadding>
           <ListItemButton divider={true} >
-                <ListItemText primary={`${boxList[index].name}`} secondary={`${boxList[index].description}`} />
-                <ListItemText primary={`Quantity: ${boxList[index].quantity}`} align='end'/>
+            <ListItemText primary={`${boxList[index].name}`} secondary={`${boxList[index].description}`} />
+            <ListItemText primary={`Quantity: ${boxList[index].quantity}`} align='end' />
           </ListItemButton>
         </ListItem>
       </>
@@ -34,50 +33,56 @@ export default function BoxPage() {
 
   return (
     <>
-    <Box sx={{ boxShadow: 0 }}>
-      <Box style={boxShadow: '0px -1.95px 1.5px rgba(0, 0, 255, .2), 1.95px 0px 1.5px rgba(0, 0, 255, .2), -1.95px 0px 1.5px rgba(0, 0, 255, .2);'}
+
+      <Box
         sx={{
-          width: '100%', height: '100%', maxWidth: '80%', bgcolor: 'background.paper', border: '1px solid #D3D3D3', margin: 'auto', marginTop: '1em', paddingLeft: '1em', boxSizing: 'border-box', boxShadow: 1,
+          width: '100%', height: '100%', maxWidth: '80%', bgcolor: 'background.paper', border: '1px solid #D3D3D3', margin: 'auto', marginTop: '1em', boxSizing: 'border-box', boxShadow: 3,
         }}
       >
-        <h1>Box Name:</h1>
-        <p>
+        <h1 style={{ paddingLeft: '0.65em' }}>Box Name:</h1>
+        <p style={{ paddingLeft: '1em' }}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
           nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
-        <AutoSizer>
-          {({ height, width }) => (
-            <FixedSizeList
-              height={200}
-              width={width}
-              itemSize={70}
-              itemCount={boxList.length}
-              overscanCount={5}
-            >
-              {renderRow}
-            </FixedSizeList>
-          )}
-        </AutoSizer>
-        </Box>
-        <Box
+        </p>
+        <List
           sx={{
-            width: '100%', height: '200px', maxWidth: '80%', bgcolor: 'background.paper', border: '1px solid #D3D3D3', margin: 'auto', boxSizing: 'border-box', boxShadow: 1,
-          }}>
-      </Box>
-     </Box>
+            width: '100%',
+            maxWidth: 800,
+            bgcolor: 'background.paper',
+            position: 'relative',
+            overflow: 'auto',
+            maxHeight: 300,
+            '& ul': { padding: 0 },
+          }}
+          subheader={<li />}
+        >
+            <li>
+              <ul>
+                {boxList.map((item, index) => (
+                  <ListItem key={index} component="div" disablePadding>
+                  <ListItemButton divider={true} >
+                    <ListItemText primary={`${boxList[index].name}`} secondary={`${boxList[index].description}`} />
+                    <ListItemText primary={`Quantity: ${boxList[index].quantity}`} align='end' />
+                  </ListItemButton>
+                </ListItem>
+                ))}
+              </ul>
+            </li>
 
-        <Box
-          sx={{
-            width: '100%', height: '100%', maxWidth: '80%', bgcolor: 'background.paper', margin: 'auto', boxSizing: 'border-box',
-          }}>
+        </List>
+      </Box>
+      <Box
+        sx={{
+          width: '100%', height: '100%', maxWidth: '80%', bgcolor: 'background.paper', margin: 'auto', boxSizing: 'border-box',
+        }}>
         <h1>Select a plan:</h1>
-        <MealList/>
+        <MealList />
         <Grid container>
           <Grid item xs={12} align='center' paddingTop='1rem'>
-        <Button className={classes.addToCart} startIcon={<AddShoppingCartIcon />}>
-          Add to Cart
-        </Button>
+            <Button className={classes.addToCart} startIcon={<AddShoppingCartIcon />}>
+              Add to Cart
+            </Button>
           </Grid>
         </Grid>
       </Box>
