6680594b1914e7235f671226be9e26386d37fc30
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-18 17:38:14 -0700

    design login/signup form

--- client/src/components/Pages/LoginPage/LoginPage.jsx
+++ client/src/components/Pages/LoginPage/LoginPage.jsx
@@ -1,22 +1,16 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import {
-  TextField,
-  Grid,
-  Paper,
-  AppBar,
-  Typography,
-  Toolbar,
-  Link,
-  Box,
-} from '@material-ui/core';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import styled from 'styled-components';
+import { Grid, Typography } from '@material-ui/core';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 import useMainContext from '../../../context/MainContext.jsx';
 import useAuth from '../../../context/AuthContext.jsx';
 import { HOME } from '../../../config/pageRoutes';
 import useStyles from '../../styles';
-import styled from 'styled-components';
+import AccountForm from './AccountForm.jsx';
 
 const LoginFormFields = [
   {
@@ -44,37 +38,13 @@ const SignupFormFields = [
   },
 ];
 
-const FormContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 3rem;
-  margin-bottom: 2rem;
-  align-items: center;
-  justify-content: center;
-`;
-
-const FormFields = ({ formEntries, fieldsArr, handleChange }) => (
-  <>
-    {fieldsArr.map(({ name, placeholder }, i) => (
-      <TextField
-        key={i}
-        onChange={handleChange}
-        type={name}
-        name={name}
-        value={formEntries[name] || ''}
-        placeholder={placeholder}
-      />
-    ))}
-  </>
-);
-
 const LoginPage = () => {
   const classes = useStyles();
 
   const [isLoginForm, setIsLoginForm] = useState(true);
   const [formEntries, setFormEntries] = useState({});
-  const [customerType, setCustomerType] = useState('customer');
-  // const [isFarmerUser, setIsFarmerUser] = useState(false);
+  const [customerType, setCustomerType] = useState('');
+  const [typeSelection, setTypeSelection] = useState('');
   const history = useHistory();
 
   const {
@@ -88,8 +58,12 @@ const LoginPage = () => {
 
   const { userType, setUserType } = useMainContext();
 
+  const handleTypeChoose = () => {
+    setCustomerType(typeSelection);
+  };
+
   const handleTypeChange = (e) => {
-    setCustomerType(e.target.value);
+    setTypeSelection(e.target.value);
   };
 
   const toggleLoginSignup = () => {
@@ -129,177 +103,70 @@ const LoginPage = () => {
     sendPasswordResetEmail(email || window.prompt('Enter your email:'));
   };
 
+  const formProps = {
+    handleChange,
+    formEntries,
+    handleTypeChange,
+    customerType,
+    handleAccountSubmit,
+    signInWithGoogle,
+    signInWithFacebook,
+    toggleLoginSignup,
+  };
+
   return (
-
     <>
-      {!isLoginForm ? (
-        <>
-          <label>
-            Account Type
-            <select value={customerType} onChange={handleTypeChange}>
-              <option value={'customer'}>
-                Customer
-              </option>
-              <option value={'farmer'}>
-                Farmer
-              </option>
-              <option value={'nutritionist'}>
-                Nutritionist
-              </option>
-            </select>
-          </label>
-
-          <FormContainer>
-            <Typography variant="h4" gutterBottom component="div">Sign Up Page</Typography>
-            <form>
-              <FormFields
-                fieldsArr={SignupFormFields}
-                handleChange={handleChange}
-                formEntries={formEntries}
-              />
-              <Button variant="contained" onClick={handleEmailPassSubmit}>Continue</Button>
-            </form>
-            <Typography variant="body1">Already have an account?</Typography><a href="/" onClick={(e) => { e.preventDefault(); toggleLoginSignup(); }}>Log In</a>
-          </FormContainer>
-        </>
+      {!customerType ? (
+        <Grid container alignItems="center" justify="space-between">
+          <Grid item style={{ margin: '2em' }}>
+            <label>
+              <Typography variant="body1">To log in or sign up, please select account type below:</Typography>
+              <Select value={typeSelection} onChange={handleTypeChange} label="Customer Type">
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={'customer'}>
+                  Customer
+                </MenuItem>
+                <MenuItem value={'farmer'}>
+                  Farmer
+                </MenuItem>
+                <MenuItem value={'nutritionist'}>
+                  Nutritionist
+                </MenuItem>
+              </Select>
+            </label>
+            <Button onClick={handleTypeChoose} variant="contained">Continue</Button>
+          </Grid>
+        </Grid>
       ) : (
         <>
-          <label>
-            Account Type
-            <select value={customerType} onChange={handleTypeChange}>
-              <option value={'customer'}>
-                Customer
-              </option>
-              <option value={'farmer'}>
-                Farmer
-              </option>
-              <option value={'nutritionist'}>
-                Nutritionist
-              </option>
-            </select>
-          </label>
-
-          <FormContainer>
-            <Typography variant="h4" gutterBottom component="div">Login Page</Typography>
-
-            <form>
-              <FormFields
-                fieldsArr={LoginFormFields}
-                handleChange={handleChange}
-                formEntries={formEntries}
-              />
-            </form>
-            <Button variant="contained" onClick={handleEmailPassSubmit}>Continue</Button>
-
-            <Typography variant="body1">No account? </Typography>
-            <Button onClick={(e) => { e.preventDefault(); toggleLoginSignup(); }}>Sign Up</Button>
-            <Button onClick={(e) => { e.preventDefault(); handleForgotPassword(); }}>
-              Forgot your password?
-            </Button>
-          </FormContainer>
+          <Button onClick={() => setCustomerType('')} variant="contained">Back</Button>
+          {!isLoginForm ? (
+            <AccountForm
+              fieldsArr={SignupFormFields}
+              title="Sign Up"
+              typeMessage="Already have an account?"
+              redirectName="Log In"
+              {...formProps}
+            >
+            </AccountForm>
+          ) : (
+            <AccountForm
+              fieldsArr={LoginFormFields}
+              title="Log In"
+              typeMessage="No account?"
+              redirectName="Sign Up"
+              {...formProps}
+            >
+              <Button onClick={(e) => { e.preventDefault(); handleForgotPassword(); }}>
+                Forgot your password?
+              </Button>
+            </AccountForm>
+          )}
         </>
       )}
-
-      <hr></hr>
-      <FormContainer>
-        <p><Typography variant="body1">OR</Typography></p>
-        <Stack spacing={2}>
-          <Button variant="outlined" size="small" style={{ maxWidth: '120px' }} onClick={() => handleAccountSubmit(signInWithGoogle)}>
-            Sign In With Google
-          </Button>
-          <Button variant="outlined" size="small" style={{ maxWidth: '120px' }} onClick={() => handleAccountSubmit(signInWithFacebook)}>
-            Sign In With Facebook
-          </Button>
-          {/* <button onClick={signInWithTwitter}> Sign In With Twitter</button> */}
-        </Stack>
-      </FormContainer>
-    </>
-  );
+    </>);
 };
 
 export default LoginPage;
-
-// <div>
-//   <AppBar position="static" alignitems="center" color="primary">
-//     <Toolbar>
-//       <Grid container justify="center" wrap="wrap">
-//         <Grid item>
-//           <Typography variant="h6">{BRAND_NAME}</Typography>
-//         </Grid>
-//       </Grid>
-//     </Toolbar>
-//   </AppBar>
-//   <Grid container spacing={0} justify="center" direction="row">
-//     <Grid item>
-//       <Grid
-//         container
-//         direction="column"
-//         justify="center"
-//         spacing={2}
-//         className="login-form"
-//       >
-//         <Paper
-//           variant="elevation"
-//           elevation={2}
-//           className="login-background"
-//         >
-//           <Grid item>
-//             <Typography component="h1" variant="h5">
-//               Sign in
-//             </Typography>
-//           </Grid>
-//           <Grid item>
-//             <form onSubmit={this.handleSubmit}>
-//               <Grid container direction="column" spacing={2}>
-//                 <Grid item>
-//                   <TextField
-//                     type="email"
-//                     placeholder="Email"
-//                     fullWidth
-//                     name="username"
-//                     variant="outlined"
-//                     value={this.state.username}
-//                     onChange={ }
-//                     required
-//                     autoFocus
-//                   />
-//                 </Grid>
-//                 <Grid item>
-//                   <TextField
-//                     type="password"
-//                     placeholder="Password"
-//                     fullWidth
-//                     name="password"
-//                     variant="outlined"
-//                     value={this.state.password}
-//                     onChange={(event) =>
-//                       this.setState({
-//                         [event.target.name]: event.target.value,
-//                       })
-//                     }
-//                     required
-//                   />
-//                 </Grid>
-//                 <Grid item>
-//                   <Button
-//                     variant="contained"
-//                     color="primary"
-//                     type="submit"
-//                     className="button-block"
-//                   >
-//                     Submit
-//                   </Button>
-//                 </Grid>
-//               </Grid>
-//             </form>
-//           </Grid>
-//           <Grid item>
-//             <Link href="#" variant="body2">
-//               Forgot Password?
-//             </Link>
-//           </Grid>
-//         </Paper>
-//       </Grid>
-//     </Grid>
-//   </Grid>
-// </div>
