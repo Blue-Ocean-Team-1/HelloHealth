f25494805f095fd86d6a88162fd6b886c5924263
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-19 19:38:50 -0700

    fix login tests

--- client/src/components/Pages/UserAccountPage/UserAccountPage.jsx
+++ client/src/components/Pages/UserAccountPage/UserAccountPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import { Grid, Paper, Box } from '@material-ui/core';
 import useAuth from '../../../context/AuthContext.jsx';
 import { TRANSACTION } from '../../../config/pageRoutes';
 
@@ -21,7 +22,13 @@ const UserAccountPage = () => {
   ];
 
   return (
-    <>
+    <Paper style={{ padding: '0.5em' }}>
+      <Box sx={{ x: 2, float: 'right' }}>
+        <Button variant="contained" onClick={() => logoutUser()}>
+          Log Out
+        </Button>
+      </Box>
+
       <h1>Account Details</h1>
       <figure>
         <h4>Available Credit: {'$20'}</h4>
@@ -30,34 +37,41 @@ const UserAccountPage = () => {
 
       <h5>{name}</h5>
 
-      <figure>
-        <h3>Subscription</h3>
-        {/* HERE */}
-        <select>
-          <option value={'active'}>Active</option>
-          <option value={'deactived'}>Deactived</option>
-        </select>
-      </figure>
+      <Box component={Grid} container boxShadow={1}>
+        <Grid>
+          <h3>Subscription</h3>
+        </Grid>
 
-      <figure>
-        <h3>Transactions</h3>
+        <Grid item>
+          <select>
+            <option value={'active'}>Active</option>
+            <option value={'deactived'}>Deactived</option>
+          </select>
+        </Grid>
+      </Box>
+
+      <Box component={Grid} container boxShadow={1}>
+        <Grid container>
+          <h3>Transactions</h3>
+        </Grid>
+
         {transactions.length === 0 ? (
-          <p>No transactions found..</p>
+          <Grid item>
+            <p>No transactions found..</p>
+          </Grid>
         ) : (
           <>
             {transactions.map(({ id }) => (
-              <div key={id}>
+              <Box key={id}>
                 <p>Transaction ID: {id}</p>
                 <p>Total: {'$14.99'}</p>
                 <Link to={`${TRANSACTION}?=${id}`}>View</Link>
-              </div>
+              </Box>
             ))}
           </>
         )}
-      </figure>
-
-      <Button onClick={() => logoutUser()}>Log Out</Button>
-    </>
+      </Box>
+    </Paper>
   );
 };
 
