27f01f3ab6c65087c234754240645dfb40672a72
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-23 12:24:07 -0700

    clean up jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,50 +1,40 @@
 module.exports = {
+  collectCoverage: true,
+  coverageReporters: ['json', 'html', 'lcov'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/*.test.js/',
+    '/config/',
+    '/test-utils/',
+  ],
   projects: [
     {
-      displayName: 'dom',
+      displayName: 'DOM',
       testEnvironment: 'jsdom',
-      // "extensionsToTreatAsEsm": [".jsx"],
+      setupFilesAfterEnv: ['<rootDir>/client/src/utils/test-utils.js'],
+      // 'extensionsToTreatAsEsm': ['.jsx'],
       testMatch: ['<rootDir>/client/**/*.test.js?(x)'],
-      transformIgnorePatterns: ['node_modules/(?!.*?/es/.*\\.js)'],
+      setupFiles: ['dotenv/config'],
+      modulePaths: ['/client/src/'],
+      moduleDirectories: ['node_modules', 'client/src'],
+      watchPlugins: [
+        'jest-watch-typeahead/filename',
+        'jest-watch-typeahead/testname',
+      ],
+      moduleFileExtensions: ['js'],
       moduleNameMapper: {
         '\\.(css|less)$': '<rootDir>/client/src/__mocks__/styleMock.js',
         '\\.(png|gif|ttf|eot|svg|mp3|jpg)$':
           '<rootDir>/client/src/__mocks__/fileMock.js',
       },
-      // collectCoverage: true,
-      // coverageReporters: [
-      //   'json',
-      //   'html',
-      //   'lcov',
-      // ],
-      // setupFilesAfterEnv: [
-      //   '<rootDir>/server/test-utils/suiteSetup.js',
-      //   '<rootDir>/client/test-utils/setupSuite.js',
-      // ],
-      // coveragePathIgnorePatterns: [
-      //   '/node_modules/',
-      //   '/*.test.js/',
-      // ],
+      transformIgnorePatterns: ['node_modules/(?!.*?/es/.*\\.js)'],
     },
     {
       displayName: 'node',
       testEnvironment: 'node',
       testMatch: ['<rootDir>/server/**/*.test.js?(x)'],
       transformIgnorePatterns: ['node_modules/(?!.*?/es/.*\\.js)'],
-      // collectCoverage: true,
-      // coverageReporters: [
-      //   'json',
-      //   'html',
-      //   'lcov',
-      // ],
-      // setupFilesAfterEnv: [
-      //   '<rootDir>/server/test-utils/suiteSetup.js',
-      //   '<rootDir>/client/test-utils/setupSuite.js',
-      // ],
-      // coveragePathIgnorePatterns: [
-      //   '/node_modules/',
-      //   '/*.test.js/',
-      // ],
+      setupFilesAfterEnv: ['<rootDir>/server/test-utils/suiteSetup.js'],
     },
   ],
 };
